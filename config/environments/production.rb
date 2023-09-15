aaae59e35acebb6af19ac57f2428550b8a214358
berylcl <gakiiberyl@gmail.com>
2023-09-15 10:23:02 +0100

    Deploying to render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,3 @@ Rails.application.configure do
 end
 # Disable serving static files from the `/public` folder by default since
 # Apache or NGINX already handles this.
-config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
